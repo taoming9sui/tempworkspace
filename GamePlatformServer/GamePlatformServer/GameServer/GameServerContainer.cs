c2f3efd078491bf23178fecce197472b5d3d494b
taoming9sui <godone545300730@163.com>
2020-01-01 00:02:09 +0800

    v0.0.8 2020跨年整合

---
+++ GamePlatformServer/GamePlatformServer/GameServer/GameServerContainer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GamePlatformServer.GameServer.ServerObjects;
+using GamePlatformServer.GameServer.GameModuels;
+using GamePlatformServer.Utils;
+
+namespace GamePlatformServer.GameServer
+{
+    public class GameServerContainer
+    {
+        private int m_socketPort;
+        private string m_socketPath;
+        private string m_sqliteConnStr;
+        private GameCenter m_center;
+        private GameClientAgent m_clientAgent;
+
+        private GamePlayerDBAgent m_playerDBAgent;
+
+
+        public GameCenter Center { get { return m_center; } }
+        public GameClientAgent ClientAgent { get { return m_clientAgent; } }
+        public GamePlayerDBAgent PlayerDBAgent { get { return m_playerDBAgent; } }
+
+        public GameServerContainer(int port, string path, string connStr)
+        {
+            m_socketPort = port;
+            m_socketPath = path;
+            m_sqliteConnStr = connStr;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                //开启GamePlayerDBAgent
+                LogHelper.LogInfo("初始化数据库在" + m_sqliteConnStr);
+                m_playerDBAgent = new GamePlayerDBAgent(m_sqliteConnStr);
+                m_playerDBAgent.Start();
+                //开启GameCenter
+                LogHelper.LogInfo("初始化GameCenter……");
+                m_center = new GameCenter(this);
+                m_center.Start();
+                //开启GameClientAgent
+                LogHelper.LogInfo(string.Format("初始化GameClientAgent在{0}", m_socketPort));
+                m_clientAgent = new GameClientAgent(this, m_socketPort, m_socketPath);
+                m_clientAgent.Start();
+                //服务启动
+                LogHelper.LogInfo("服务器启动成功！");
+            }
+            catch(Exception ex)
+            {
+                LogHelper.LogError(ex.Message + "|" + ex.StackTrace);
+                LogHelper.LogInfo("服务器启动失败:" + ex.Message);
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                //关闭GameClientAgent
+                if(m_clientAgent != null)
+                    m_clientAgent.Stop();
+                //关闭GameCenter
+                if (m_center != null)
+                    m_center.Stop();
+                //关闭GamePlayerDBAgent
+                if(m_playerDBAgent != null)
+                    m_playerDBAgent.Stop();
+                //关闭成功
+                LogHelper.LogInfo("服务器已关闭�?");
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogError(ex.Message + "|" + ex.StackTrace);
+                LogHelper.LogInfo("服务器关�?出错:" + ex.Message);
+            }
+
+        }
+
+    }
+}
