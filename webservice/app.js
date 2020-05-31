d27b289a1d52201a151bbd5c6afaa93b920cf26b
taoming9sui <godone545300730@163.com>
2020-05-31 19:42:04 +0800

    deleted unnecessary files

--- webservice/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-app.use('/static', express.static(path.join(__dirname, 'static')));
